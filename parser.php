18a442877a476be1369d4e91025a31dd2c9796e7
Youi <igonejack@gmail.com>
2016-06-10 22:58:44 +0800

    Update: minor

--- parser.php
+++ parser.php
@@ -14,11 +14,11 @@ class PARSER {
     }
 
     public static function Answer($JSON) {
-        $Q = static::_decodeHTMLChars($JSON['question']);
-        $A   = static::_decodeHTMLChars($JSON['answer']);
-        $TAGs     = implode(', ', isset($JSON['tags']) ? $JSON['tags'] : []);
+        $Q    = static::_decodeHTMLChars($JSON['question']);
+        $A    = static::_decodeHTMLChars($JSON['answer']);
+        $TAGs = implode(', ', isset($JSON['tags']) ? $JSON['tags'] : []);
 
-        $text   = "[Q&A]\r\n\r\n$Q\r\n\r\n$A\r\n\r\nTags: $TAGs\r\n";
+        $text = "[Q&A]\r\n\r\n$Q\r\n\r\n$A\r\n\r\nTags: $TAGs\r\n";
 
         return htmlspecialchars($text);
     }
@@ -28,21 +28,21 @@ class PARSER {
         foreach ($JSON['conversation'] as $item) {
             $HTML .= "{$item['label']} {$item['phrase']}\r\n";
         }
-        $DATE = "date: {$JSON['date']}";
+        $date = "date: {$JSON['date']}";
         $URL  = "url: {$JSON['url']}";
-        $TAGs = 'tags: ' . implode(', ', isset($JSON['tags']) ? $JSON['tags'] : []);
+        $tags = 'tags: ' . implode(', ', isset($JSON['tags']) ? $JSON['tags'] : []);
 
-        $HTML = "$HTML\r\n$DATE\r\n$TAGs\r\n$URL";
+        $HTML = "$HTML\r\n$date\r\n$tags\r\n$URL";
 
         return nl2br($HTML);
     }
 
     public static function Link($JSON) {
         $HTML = <<< EOD
-                        <p>Title: <h3>{$JSON['link-text']}</h3></p>
-                        <p>link: <a href="{$JSON['link-url']}">{$JSON['link-url']}</a></p>
-                        <p>Description:</p>
-                        <p>{$JSON['link-description']}</p>
+            <p>Title: <h3>{$JSON['link-text']}</h3></p>
+            <p>link: <a href="{$JSON['link-url']}">{$JSON['link-url']}</a></p>
+            <p>Description:</p>
+            <p>{$JSON['link-description']}</p>
 EOD;
 
         return $HTML;
@@ -74,7 +74,6 @@ EOD;
     }
 
     public static function Video($JSON) {
-
         $e = new Exception('NO VIDEO SOURCE PARSED');
 
         $vSource = $JSON['video-source'];
@@ -89,18 +88,18 @@ EOD;
                 return "http://vt.tumblr.com/$vID.mp4";
             }
 
-            throw $e;
+            else throw $e;
         }
 
-        if (preg_match('<src="(.+?)">', $vSource, $match)) {
+        else if (preg_match('<src="(.+?)">', $vSource, $match)) {
             return $match[1];
         }
 
-        if (isset($JSON['video-player']) && preg_match('<src="(.+?)">', $JSON['video-player'], $match)) {
+        else if (isset($JSON['video-player']) && preg_match('<src="(.+?)">', $JSON['video-player'], $match)) {
             return $match[1];
         }
 
-        throw $e;
+        else throw $e;
     }
 
     public static function Photo($JSON) {
